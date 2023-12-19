c8345eebdc2ac612f85e0260638276d80d33fbc6
روجر <104784913+rogerpq@users.noreply.github.com>
2023-12-19 15:39:13 +0300

    Add files via upload

---
+++ repthon/core/session.py
@@ -0,0 +1,51 @@
+import sys
+
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.errors import AccessTokenExpiredError, AccessTokenInvalidError
+from ..Config import Config
+from .bothseesion import bothseesion
+from .client import RepUserBotClient
+from .logger import logging
+
+LOGS = logging.getLogger("ريبـــثون")
+__version__ = "2.10.6"
+
+loop = None
+
+if Config.STRING_SESSION:
+    session = bothseesion(Config.STRING_SESSION, LOGS)
+else:
+    session = "baqir"
+
+try:
+    zq_lo = RepUserBotClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(
+        f"STRING_SESSION CODE WRONG MAKE A NEW SESSION - {e}\n كود سيشن تيليثـون غير صالح .. قم باستخـراج كود جديد ؟!"
+    )
+    sys.exit()
+
+try:
+    zq_lo.tgbot = tgbot = RepUserBotClient(
+        session="RepTgbot",
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    ).start(bot_token=Config.TG_BOT_TOKEN)
+except AccessTokenExpiredError:
+    LOGS.error("توكن البوت غير صالح قم باستبداله بتوكن جديد من بوت فاذر")
+except AccessTokenInvalidError:
+    LOGS.error("توكن البوت غير صحيح قم باستبداله بتوكن جديد من بوت فاذر")
