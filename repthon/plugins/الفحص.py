bc9cbe5aa8776ae319b2f0f14a4bf5e78b92cc38
روجر <104784913+rogerpq@users.noreply.github.com>
2024-01-18 19:27:58 +0300

    Update الفحص.py

--- repthon/plugins/الفحص.py
+++ repthon/plugins/الفحص.py
@@ -4,10 +4,9 @@
 import random
 import re
 import time
-import os
+import psutil
 from datetime import datetime
 from platform import python_version
-from random import choice
 
 import requests
 from telethon import version
@@ -16,66 +15,111 @@ from telethon.errors.rpcerrorlist import (
     WebpageCurlFailedError,
     WebpageMediaEmptyError,
 )
-from repthon import StartTime, zq_lo, repversion
+from telethon.events import CallbackQuery
+
+from . import StartTime, zq_lo, repversion
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
 from ..helpers.functions import repalive, check_data_base_heal_th, get_readable_time
 from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
+from . import mention
 
 plugin_category = "العروض"
-ALIVE = gvarstatus("R_ALIVE") or "فحص"
+STATS = gvarstatus("R_STATS") or "فحص"
 
-# @E_7_V - # إضافة التاريخ كتابتي غير مبري الذمة الي ياخذه
-file_path = "installation_date.txt"
-if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
-    with open(file_path, "r") as file:
-        installation_time = file.read().strip()
-else:
-    installation_time = datetime.now().strftime("%Y-%m-%d")
-    with open(file_path, "w") as file:
-        file.write(installation_time)
 
-@zq_lo.rep_cmd(pattern=f"{ALIVE}$")
-async def alive(event):
+@zq_lo.rep_cmd(pattern=f"{STATS}$")
+async def amireallyalive(event):
     reply_to_id = await reply_id(event)
     uptime = await get_readable_time((time.time() - StartTime))
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
     start = datetime.now()
-    repevent = await edit_or_reply(event, "**𓅓┊جـاري .. فحـص البـوت الخـاص بك**")
+    repevent = await edit_or_reply(event, "**𓅓┊جـارِ .. فحـص البـوت الخـاص بك**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
+    if gvarstatus("r_date") is not None:
+        rrd = gvarstatus("r_date")
+        rrt = gvarstatus("r_time")
+        repda = f"{rrd}┊{rrt}"
+    else:
+        repda = f"{bt.year}/{bt.month}/{bt.day}"
     R_EMOJI = gvarstatus("ALIVE_EMOJI") or "𓃰┊"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "بـوت ريبـــثون 𝐑𝐞𝐩𝐭𝐡𝐨𝐧 يعمـل .. بنجـاح ☑️ 𓆩"
-    RANDOM_MEDIA = ["https://graph.org/file/f4c01d51562507a36c07e.mp4","https://graph.org/file/0b1e5679e24e735f870c5.mp4","https://graph.org/file/cafa0e8a1320891a65ae2.mp4","https://graph.org/file/b442b635cecca399dea39.mp4","https://graph.org/file/534d48ffb4b1e22e4ee39.mp4","https://graph.org/file/ec26c9d0a5532f17f85ac.mp4","https://graph.org/file/5201ed73785e5a928c853.mp4","https://graph.org/file/764e2427fafbe4aec2251.mp4","https://graph.org/file/9501d29c6cccd86b22686.mp4","https://graph.org/file/e30ff8013dd3f61f0735e.mp4","https://graph.org/file/3b9dc775779767faeb774.mp4","https://graph.org/file/1ee6a852367700b272a51.mp4","https://graph.org/file/53809263bafc29ef6adee.mp4","https://graph.org/file/4c6325935cb7e5494c77e.mp4","https://graph.org/file/a6a25238f38a351da1e33.mp4"] #@rNrYr
-    ALIVE_TEXT=ALIVE_TEXT
-    R_EMOJI=R_EMOJI
-    uptime=uptime
-    telever=version.__version__
-    repver=repversion
-    pyver=python_version()
-    dbhealth=check_sgnirts
-    ping=ms
-    repthon_Tare5=installation_time # إضافة التاريخ كتابتي
-    tgbot = Config.TG_BOT_USERNAME #@rNrYr اذكر حقوق يلا تخمط حقوق أحمد-دار
-    me = await event.client.get_me()
-    my_first = me.first_name
-    my_mention = f"[{me.first_name}](tg://user?id={me.id})"
-    delete = await event.delete()
-    user = await event.client.get_entity(event.chat_id)
-    my_mention=my_mention
-        
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**بـوت ريبـــثون 𝗥𝗘𝗣𝗧𝗛𝗢𝗡  يعمـل .. بنجـاح ☑️ 𓆩 **"
+    REP_IMG = gvarstatus("ALIVE_PIC")
+    rep_caption = gvarstatus("ALIVE_TEMPLATE") or rep_temp
+    caption = rep_caption.format(
+        ALIVE_TEXT=ALIVE_TEXT,
+        R_EMOJI=R_EMOJI,
+        mention=mention,
+        uptime=uptime,
+        repda=repda,
+        telever=version.__version__,
+        repver=repversion,
+        pyver=python_version(),
+        dbhealth=check_sgnirts,
+        ping=ms,
+    )
+    if REP_IMG:
+        REP = [x for x in REP_IMG.split()]
+        PIC = random.choice(REP)
+        try:
+            await event.client.send_file(
+                event.chat_id, PIC, caption=caption, reply_to=reply_to_id
+            )
+            await repevent.delete()
+        except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
+            return await edit_or_reply(
+                zedevent,
+                f"**⌔∮ عـذراً عليـك الـرد ع صـوره او ميـديـا  ⪼  `.اضف صورة الفحص` <بالرد ع الصـوره او الميـديـا> ",
+            )
+    else:
+        await edit_or_reply(
+            repevent,
+            caption,
+        )
 
-    
-    final_message = f""" **{ALIVE_TEXT}**\n
-**{R_EMOJI} قاعدۿ البيانات :** تعمل بنـجاح
-**{R_EMOJI} إصـدار التـيليثون :** {telever}
-**{R_EMOJI} إصـدار ريبـــثون :** {repver}
-**{R_EMOJI} إصـدار البـايثون :** {pyver}
-**{R_EMOJI} الوقـت :** {uptime}
-**{R_EMOJI} المسـتخدم:** {my_mention}
-**{R_EMOJI} التـاريـخ:** {repthon_Tare5}
-**{R_EMOJI} الـبـوت: ** {tgbot}
+
+rep_temp = """{ALIVE_TEXT}
+
+**{R_EMOJI} قاعدة البيانات :** تعمـل بـ نجـاح ♾
+**{R_EMOJI} إصـدار المكتبـه :** `{telever}`
+**{R_EMOJI} إصـدار السـورس :** `{repver}`
+**{R_EMOJI} إصـدار بايثـون :** `{pyver}`
+**{R_EMOJI} وقت التشغيل :** `{uptime}`
+**{R_EMOJI} تاريـخ التنصيب :** `{repda}`
+**{R_EMOJI} المسـتخـدم:** {mention}
 **{R_EMOJI} قنـاة السـورس :** [اضغـط هنـا](https://t.me/Repthon)"""
-    send_new_message = await event.client.send_message(entity=event.chat_id, message=final_message, file=random.choice(RANDOM_MEDIA)) #اذكر الحقوق @rNrYr حقوق أحمد-دار
+
+
+@zq_lo.rep_cmd(
+    pattern="الفحص$",
+    command=("الفحص", plugin_category),
+    info={
+        "header": "- لـ التحـقق من ان البـوت يعمـل بنجـاح .. بخـاصيـة الانـلايـن ✓",
+        "الاسـتخـدام": [
+            "{tr}الفحص",
+        ],
+    },
+)
+async def amireallyialive(event):
+    "A kind of showing bot details by your inline bot"
+    reply_to_id = await reply_id(event)
+    R_EMOJI = gvarstatus("ALIVE_EMOJI") or "𓅓┊"
+    rep_caption = "** بـوت ريبـــثون 𝗥𝗘𝗣𝗧𝗛𝗢𝗡  يعمـل .. بنجـاح ☑️ 𓆩 **\n"
+    rep_caption += f"**{R_EMOJI} إصـدار التـيليثون :** `{version.__version__}\n`"
+    rep_caption += f"**{R_EMOJI} إصـدار ريبـــثون :** `{repversion}`\n"
+    rep_caption += f"**{R_EMOJI} إصـدار البـايثون :** `{python_version()}\n`"
+    rep_caption += f"**{R_EMOJI} المسـتخدم :** {mention}\n"
+    results = await event.client.inline_query(Config.TG_BOT_USERNAME, rep_caption)
+    await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+    await event.delete()
+
+
+@zq_lo.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
+async def on_plug_in_callback_query_handler(event):
+    statstext = await repalive(StartTime)
+    await event.answer(statstext, cache_time=0, alert=True)
