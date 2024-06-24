3d3d1b5c2dc0a3b287fe54a3487632be51bec265
روجر <104784913+rogerpq@users.noreply.github.com>
2024-06-24 08:51:45 +0300

    Update startup.py

--- repthon/utils/startup.py
+++ repthon/utils/startup.py
@@ -128,11 +128,6 @@ async def setup_bot():
         bot_details = await zq_lo.tgbot.get_me()
         Config.TG_BOT_USERNAME = f"@{bot_details.username}"
         # await zq_lo.start(bot_token=Config.TG_BOT_USERNAME)
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        baqirport = Config.PORT
-        await web.TCPSite(app, bind_address, baqirport).start()
         zq_lo.me = await zq_lo.get_me()
         zq_lo.uid = zq_lo.tgbot.uid = utils.get_peer_id(zq_lo.me)
         if Config.OWNER_ID == 0:
