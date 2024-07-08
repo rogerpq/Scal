cdacbe04c3e3f99d51f18ab8ad0c2f65772fdc56
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-08 12:14:40 +0300

    Update __init__.py

--- repthon/plugins/__init__.py
+++ repthon/plugins/__init__.py
@@ -1,4 +1,3 @@
-# جميع الحقوق - @Repthon
 import math
 import os
 import re
@@ -24,7 +23,7 @@ from repthon.helpers.functions.musictool import song_download
 bot = zq_lo
 LOGS = logging.getLogger(__name__)
 USERID = zq_lo.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
-ALIVE_NAME = Config.ALIVE_NAME
+ALIVE_NAME = gvarstatus("ALIVE_NAME") if gvarstatus("ALIVE_NAME") else "-"
 
 Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
@@ -34,8 +33,8 @@ HEROKU_API_KEY = Config.HEROKU_API_KEY
 thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
 
 # mention user
-mention = f"[{Config.ALIVE_NAME}](tg://user?id={USERID})"
-hmention = f"<a href = tg://user?id={USERID}>{Config.ALIVE_NAME}</a>"
+mention = f"[{ALIVE_NAME}](tg://user?id={USERID})"
+hmention = f"<a href = tg://user?id={USERID}>{ALIVE_NAME}</a>"
 
 PM_START = []
 PMMESSAGE_CACHE = {}
