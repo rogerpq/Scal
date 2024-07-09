88683b02b43631b35dc18114d72204f1718bd40a
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-09 10:17:40 +0300

    Update bot_pms.py

--- repthon/assistant/bot_pms.py
+++ repthon/assistant/bot_pms.py
@@ -65,7 +65,7 @@ async def check_bot_started_users(user, event):
 
 
 @zq_lo.bot_cmd(
-    pattern=f"^/start({botusername})?([\s]+)?$",
+    pattern=f"^/start({botusername})?([\\s]+)?$",
     incoming=True,
     func=lambda e: e.is_private,
 )
