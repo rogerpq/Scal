59989d508ae9b61f0a8bccffb067096c4054b9a5
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-15 11:43:40 +0300

    Update and rename tede_chatbot_sql.py to tede_chatapp_sql.py

--- repthon/sql_helper/tede_chatapp_sql.py
+++ repthon/sql_helper/tede_chatapp_sql.py
@@ -5,22 +5,22 @@ from sqlalchemy import Column, String
 from . import BASE, SESSION
 
 
-class TedeChatBot(BASE):
-    __tablename__ = "tede_chatbot"
+class TedeChatapp(BASE):
+    __tablename__ = "tede_chatapp"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = str(chat_id)
 
 
-TedeChatBot.__table__.create(bind=SESSION.get_bind(), checkfirst=True)
+TedeChatapp.__table__.create(bind=SESSION.get_bind(), checkfirst=True)
 
 INSERTION_LOCK = threading.RLock()
 
 
 def is_tede(chat_id):
     try:
-        chat = SESSION.query(TedeChatBot).get(str(chat_id))
+        chat = SESSION.query(TedeChatapp).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
@@ -28,7 +28,7 @@ def is_tede(chat_id):
 
 def set_tede(chat_id):
     with INSERTION_LOCK:
-        tedechat = SESSION.query(TedeChatBot).get(str(chat_id))
+        tedechat = SESSION.query(TedeChatapp).get(str(chat_id))
         if not tedechat:
             tedechat = TedeChatBot(str(chat_id))
         SESSION.add(tedechat)
@@ -37,7 +37,7 @@ def set_tede(chat_id):
 
 def rem_tede(chat_id):
     with INSERTION_LOCK:
-        tedechat = SESSION.query(TedeChatBot).get(str(chat_id))
+        tedechat = SESSION.query(TedeChatapp).get(str(chat_id))
         if tedechat:
             SESSION.delete(tedechat)
         SESSION.commit()
