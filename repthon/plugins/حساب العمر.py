7c1ecf52e3dcd9e1d477fc750738f08932609891
روجر <104784913+rogerpq@users.noreply.github.com>
2024-07-28 08:17:45 +0300

    Update حساب العمر.py

--- repthon/plugins/حساب العمر.py
+++ repthon/plugins/حساب العمر.py
@@ -1,16 +1,21 @@
-#Repthon
+# اصلاح نـمـى🔥
 
 from datetime import datetime
-
 from repthon import zq_lo
 from ..core.managers import edit_delete, edit_or_reply
 
-
 @zq_lo.rep_cmd(pattern="حساب العمر(?:\\s|$)([\\s\\S]*)")
 async def _(event):
-    yar = event.text[12:]
-    if not yar:
-       return await edit_or_reply(event, "**✾╎استخـدم الامـر كالتالـي .. حساب العمر + السنـه**")
+    yar_str = event.text[12:]
+    if not yar_str:
+        return await edit_or_reply(event, "**✾╎استخـدم الامـر كالتالـي .. حساب العمر + السنـة**")
+    
+    # Convert input year to integer
+    try:
+        yar = int(yar_str)
+    except ValueError:
+        return await edit_or_reply(event, "**✾╎يرجى إدخال عدد صحيح للسنة**")
+    
     YearNow = datetime.now().year
     MyAge = YearNow - yar
-    await edit_or_reply(e, "**🚹╎عمرك هـو :**  {}".format(MyAge))
+    await edit_or_reply(event, "**🚹╎عمرك هـو :**  {}".format(MyAge))
